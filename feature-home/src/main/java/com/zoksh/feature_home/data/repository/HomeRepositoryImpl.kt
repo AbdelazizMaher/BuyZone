42d6481e0f55290157ec27d4ef1b5b87ca4676d8
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2026-02-14 20:37:07 +0200

    Implement `HomeRepositoryImpl` methods with network connectivity checks
    Introduced a centralized remote call execution logic in `HomeRepositoryImpl` that validates network connectivity before fetching data.
    
    Key changes:
    - **`HomeRepositoryImpl`**:
        - Added `ConnectivityObserver` dependency to monitor network status.
        - Implemented `getCategories`, `getPopularBrands`, and `getTrendingProducts` by delegating to `ShopRemoteDataSource`.
        - Introduced a private `executeRemoteCall` helper method that returns a `DataError.Network.NO_INTERNET` error if the device is offline.
        - Refactored existing `TODO` messages for consistency.

--- feature-home/src/main/java/com/zoksh/feature_home/data/repository/HomeRepositoryImpl.kt
+++ feature-home/src/main/java/com/zoksh/feature_home/data/repository/HomeRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.zoksh.feature_home.data.repository
 
+import com.zoksh.core_common.domain.connectivity.ConnectivityObserver
 import com.zoksh.core_common.domain.error.DataError
 import com.zoksh.core_common.domain.result.Result
 import com.zoksh.feature_home.data.remote.shop.ShopRemoteDataSource
@@ -9,27 +10,40 @@ import com.zoksh.feature_home.domain.model.Header
 import com.zoksh.feature_home.domain.model.Product
 import com.zoksh.feature_home.domain.model.Promo
 import com.zoksh.feature_home.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.first
 
 class HomeRepositoryImpl(
-    private val shopRemoteDataSource: ShopRemoteDataSource
+    private val shopRemoteDataSource: ShopRemoteDataSource,
+    private val connectivityObserver: ConnectivityObserver
 ) : HomeRepository {
+
     override suspend fun getHeader(): Result<Header, DataError.Network> {
-        TODO("Not yet implemented")
+        TODO()
     }
 
     override suspend fun getPromos(): Result<List<Promo>, DataError.Network> {
-        TODO("Not yet implemented")
+        TODO()
     }
 
     override suspend fun getCategories(): Result<List<Category>, DataError.Network> {
-        TODO("Not yet implemented")
+        return executeRemoteCall { shopRemoteDataSource.getCategories() }
     }
 
     override suspend fun getPopularBrands(): Result<List<Brand>, DataError.Network> {
-        TODO("Not yet implemented")
+        return executeRemoteCall { shopRemoteDataSource.getBrands() }
     }
 
     override suspend fun getTrendingProducts(): Result<List<Product>, DataError.Network> {
-        TODO("Not yet implemented")
+        return executeRemoteCall { shopRemoteDataSource.getTrendingProducts() }
+    }
+
+
+    private suspend fun <T> executeRemoteCall(
+        call: suspend () -> Result<T, DataError.Network>
+    ): Result<T, DataError.Network> {
+        if (!connectivityObserver.isConnected.first()) {
+            return Result.Error(DataError.Network.NO_INTERNET)
+        }
+        return call()
     }
 }
